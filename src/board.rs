c874cfb648eca7ae589403b89d6b791c6a5eb04e
Jeremy Fitzhardinge <jeremy@goop.org>
2016-03-23 00:29:46 -0700

    Board display/parse helpers, tests for play()

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,9 @@
 use std::collections::{HashSet};
 use std::collections::hash_map::{HashMap};
 use std::iter::{self, FromIterator};
+use std::cmp::max;
+use std::str::FromStr;
+use std::fmt::{self, Display};
 
 use stone::Stone;
 use group::{Group, GroupIterator};
@@ -126,5 +129,141 @@ impl Board {
             .filter(|l| self.get(l).is_none())
             .map(|l| *l)
             .collect()
+    }
+}
+
+impl FromStr for Board {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Board, ()> {
+        // Given a single string containing one row per line:
+        //    . . . # . O
+        //    . . . # # O
+        // generate a Board containing that position.
+        //
+        // The board is always upper-left. The dimensions are max(width,height)
+        // of the text rows.
+        //
+        // In each row, spaces are ignored, '.' is a blank space, # is black,
+        // O is white.
+
+        let layout: Vec<Vec<Option<Stone>>> =
+            s.lines()
+                .map(|l| l.chars()
+                            .filter_map(|c|
+                                match c {
+                                    '.'         => Some(None),
+                                    '#' | 'X'   => Some(Some(Stone::Black)),
+                                    'O' | 'o'   => Some(Some(Stone::White)),
+                                    _ => None,
+                                })
+                            .collect())
+                .collect();
+
+        let w = layout.iter().map(|r| r.len()).max().unwrap_or(0);
+        let h = layout.len();
+        let sz = max(w, h);
+
+        let mut board = Board::new_with_size(sz);
+
+        for (rnum, row) in layout.into_iter().enumerate() {
+            for (cnum, stone) in row.into_iter().enumerate() {
+                let loc = Location::from((cnum, sz - rnum));
+
+                if let Some(s) = stone {
+                    let _ = board.add(loc, s);
+                }
+            }
+        }
+
+        Ok(board)
+    }
+}
+
+impl Display for Board {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let sz = self.size();
+        for row in 0..sz {
+            for col in 0..sz {
+                let loc = Location::new(col, sz-row-1);
+                let c =
+                    match self.get(loc) {
+                        None =>                 '.',
+                        Some(Stone::Black) =>   '#',
+                        Some(Stone::White) =>   'O',
+                    };
+                try!(write!(fmt, "{} ", c));
+            }
+            try!(write!(fmt, "\n"));
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::Board;
+    use location::Location;
+    use stone::Stone::{Black, White};
+
+    #[test] fn fromstr() {
+        let b = Board::from_str("
+        . . . # O O
+        . . . # # O
+        . . . . O #
+        . . . . . . .
+        ").expect("failed");
+        println!("Board:\n{}", b);
+        let bstr = format!("{}", b);
+        assert_eq!(bstr, "\
+. . . # O O . \n\
+. . . # # O . \n\
+. . . . O # . \n\
+. . . . . . . \n\
+. . . . . . . \n\
+. . . . . . . \n\
+. . . . . . . \n\
+");
+    }
+
+    #[test] fn play() {
+        let mut b = Board::new_with_size(5);
+        assert!(b.play(Location::new(0,0), Black));
+
+        let bstr = format!("{}", b);
+        assert_eq!(bstr, "\
+. . . . . \n\
+. . . . . \n\
+. . . . . \n\
+. . . . . \n\
+# . . . . \n\
+");
+
+        assert!(!b.play(Location::new(0,0), Black));
+        assert!(!b.play(Location::new(0,0), White));
+
+        assert!(b.play(Location::new(1,0), White));
+        assert!(b.play(Location::new(1,2), Black));
+        assert!(b.play(Location::new(0,1), White)); // capture
+
+        assert!(b.play(Location::new(0,2), Black));
+        assert!(b.play(Location::new(1,1), White));
+        assert!(b.play(Location::new(2,0), Black));
+        assert!(b.play(Location::new(3,3), White));
+        assert!(b.play(Location::new(2,1), Black));
+        assert!(b.play(Location::new(3,2), White));
+        assert!(b.play(Location::new(0,0), Black)); // capture
+
+        let bstr = format!("{}", b);
+        assert_eq!(bstr, "\
+. . . . . \n\
+. . . . . \n\
+# #   . . \n\
+. . # . . \n\
+# . # . . \n\
+");
+
+        println!("Board:\n{}", b)
     }
 }
